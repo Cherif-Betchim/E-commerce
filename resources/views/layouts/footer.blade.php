f3392b95441dd45c1b66ef49d5b5ac9cded6e0ec
Tommy Vang <tommy.vang@Digital-Grenoble.local>
2019-02-28 11:52:40 +0100

    Add layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+    <div class="container">
+        <div class="rs">
+            <a href="#" class="footer-picto black" title="Liens vers Facebook">
+                <img src="img/facebook.svg" alt="Pictogramme Facebook" class="black">
+                <img src="img/facebook-white.svg" alt="Pictogramme Facebook" class="white">
+            </a>
+            <a href="#" class="footer-picto black" title="Liens vers Twitter">
+                <img src="img/twitter.svg" alt="Pictogramme Twitter" class="black">
+                <img src="img/twitter-white.svg" alt="Pictogramme Twitter" class="white">
+            </a>
+            <a href="#" class="footer-picto black" title="Liens vers Linkedin">
+                <img src="img/linkedin.svg" alt="Pictogramme Linkedin" class="black">
+                <img src="img/linkedin-white.svg" alt="Pictogramme Linkedin" class="white">
+            </a>
+
+
+        </div>
+        <div class="logo">
+            <a href="index.html">
+                <img src="img/logo-k4.png" alt="Logo KitKatre">
+            </a>
+        </div>
+        <div class="mention-legale">
+            <a href="#" title="Liens vers les mentions légales">Mentions légales</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,53 @@
+<header class="header">
+    <div class="container-fluid">
+        <a href="index.html" class="logo" title="Logo"><img src="img/logo-k4.png" alt="logo"></a>
+        <nav class="nav">
+            <ul class="menu">
+                <li>
+                    <a href="index.html" class="header-texte">Laravel e-commerce</a>
+                </li>
+
+                <li class="produits">
+                    <a href="produits.html" class="header-texte">PRODUITS</a>
+
+                    <ul class="sous-menu-prod">
+                        <li>
+                            <a href="">Produit 1</a>
+                        </li>
+                        <li>
+                            <a href="">Produit 2</a>
+                        </li>
+                        <li>
+                            <a href="">Produit 3</a>
+                        </li>
+                        <li>
+                            <a href="">Produit 4</a>
+                        </li>
+                    </ul>
+                </li>
+
+                {{-- <li class="team">
+                    <a href="team.html" class="header-texte">EQUIPE </a>
+                    <ul class="sous-menu-team">
+                        <li>
+                            <a href="sites/pinel-marlene/cv-index.html">Chloé</a>
+                        </li>
+                        <li>
+                            <a href="sites/dobon-fabien/cv2.html">Fabien</a>
+                        </li>
+                        <li>
+                            <a href="sites/vang-tommy/index.html">Tommy</a>
+                        </li>
+                        <li>
+                            <a href="sites/virat-matthias/CV_principale.html">Matthias</a>
+                        </li>
+                    </ul>
+                </li> --}}
+
+                <li>
+                    <a href="contact.html" class="header-texte">CONTACT </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
