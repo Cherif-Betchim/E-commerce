204a61dc1c294bc8bbf31193838c8b429b402606
Chérif Benali <cherif.benali@Digital-Grenoble.local>
2019-03-01 17:00:06 +0100

    modify checkout route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 
 
-Route::get('/','CheckoutController@show');
+Route::get('/checkout','CheckoutController@show');
 
 Route::get('/products', 'ProductsController@show');
 
