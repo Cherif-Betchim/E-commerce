743b503375af05c3a1710bcfa4dc1a203b36f2bf
Tommy Vang <tommy.vang@Digital-Grenoble.local>
2019-03-18 17:12:55 +0100

    Add Bootstrap on layout and modify welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,25 @@
 
 @section('content')
     <div class="welcome">
-    <h1>Bienvenue sur notre site ILOT-3 !</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="login col-lg-4">
+                    <div class="container">
+                        <p>
+                            Pour optimiser votre shopping, commencez par vous connecter ou vous créer un compte.
+                        </p>
+                        <a href="" class="btn">
+                            Se Connecter
+                        </a>
+                        <a href="" class="btn">
+                            Créer un compte
+                        </a>
+                    </div>
+                </div>
+                <div class="hello col-lg-8">
+                    <h1>Bienvenue sur notre <span class="logo-color">ILOT-3</span> !</h1>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
