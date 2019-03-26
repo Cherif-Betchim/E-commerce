b5ab5373e30ef081d9b6320147ab14c0fdbf4370
Chloé Briquet <chloe.briquet@le-campus-numerique.fr>
2019-03-26 16:13:53 +0100

    modify migration file to put back is_admin column in users table

--- database/migrations/2019_03_26_135442_update_users_table.php
+++ database/migrations/2019_03_26_135442_update_users_table.php
@@ -15,6 +15,7 @@ class UpdateUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('first_name')->after('id');
+            $table->boolean('is_admin')->default(false)->after('password');
         });
     }
 
@@ -26,7 +27,7 @@ class UpdateUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('first_name');
+            $table->dropColumn(['first_name', 'is_admin']);
         });
     }
 }
