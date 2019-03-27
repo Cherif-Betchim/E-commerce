100cd59f88b8026f093f8e5f033949d22ec2ef69
jeromeVitali <jerome.vitali@le-campus-numerique.fr>
2019-03-27 11:23:38 +0100

    change my account page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,97 +1,40 @@
 @extends('layout.layout')
 
-@section('title','Accueil')
+@section('title','Mon compte')
 
 @section('content')
-    <div class="title m-b-md">
-        <h1>Mon compte</h1>
-    </div>
 
-    <div class="my-data">
-        <p>
-            Informations relatives à votre compte
-        </p>
-        <div class="view-data">
-            <div class="view-civilite">
-                <p>
-                    Nom :
-                </p>
-                <p>
-                    Prénom :
-                </p>
-                <p>
-                   Mes adresses :
-                </p>
-                <p>
-                    Code postal :
-                </p>
-                <p>
-                    Pays :
-                </p>
-                <br>
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#profile" data-toggle="tab">Mon Profil</a> </li>
+        <li><a href="#adresses" data-toggle="tab">Mes adresses</a> </li>
+        <li><a href="#myorders" data-toggle="tab">Mes commandes</a> </li>
+        <li><a href="#changepassword" data-toggle="tab">Changer mdp</a> </li>
+    </ul>
 
-            </div>
+    <div class="tab-content">
+        <div id="profile" class="tab-pane fade in active">
+            <form action="" method="post">
+                <label>Prenom</label>
+                <input type="text" name="first_name" value="{{Auth::user()->first_name}}">
+                <label>Nom</label>
+                <input type="text" name="last_name" value="{{Auth::user()->last_name}}">
+                <label>Email</label>
+                <input type="text" name="email" value="{{Auth::user()->email}} ">
+                </br>
+            </form>
         </div>
 
+        <div id="adresses" class="tab-pane fade in">
+            Mes adresses tab
+        </div>
 
-        <div class="orders">
-            <h2>Vos commandes</h2>
-            <h3>Dernière commande</h3>
-            <div class="recent-orders">
-                {{-- Affichage commande récente --}}
-                <div class="orders-img">
-                    <p>
-                        Image produit
-                    </p>
-                </div>
-                <div class="orders-ref">
-                    <p>
-                        Référence de la commande :
-                    </p>
-                    <p>
-                        Prix commande :
-                    </p>
-                </div>
-                <div class="orders-product-desc">
-                    <h3 class="orders-title">
-                        Titre Produit
-                    </h3>
-                    <p class="orders-desc">
-                        Description produit
-                    </p>
-                    <p class="orders-price">
-                        Prix produit
-                    </p>
-                </div>
-            </div>
-            <h3>Toutes vos commandes</h3>
-            <div class="all-orders">
-                {{-- Affichage de toutes les commandes passées --}}
-                <div class="orders-ref">
-                        <p>
-                            Référence de la commande :
-                        </p>
-                </div>
-                <div class="orders-price">
-                    <p>
-                        Prix commande :
-                    </p>
-                </div>
-                <div class="orders-product-desc">
-                    <h3 class="orders-title">
-                        Titre Produit
-                    </h3>
-                    <p class="orders-desc">
-                        Description produit
-                    </p>
-                    <p class="orders-price">
-                        Prix produit
-                    </p>
-                </div>
-            </div>
-            <div class="return-product">
-                <a href="" class="btn">Imprimer le bon de retour</a>
-            </div>
+        <div id="myorders" class="tab-pane fade in">
+            Mes commandes tab
+        </div>
+
+        <div id="changepassword" class="tab-pane fade in">
+            Change mdp tab
         </div>
     </div>
+
 @endsection
