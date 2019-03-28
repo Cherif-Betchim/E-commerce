2b0951ddbe984a9a01cad99e267792ba4ce270eb
jeromeVitali <jerome.vitali@le-campus-numerique.fr>
2019-03-28 11:38:45 +0100

    updated comments controller

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Comment;
 use App\Product;
-use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
